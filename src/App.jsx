d412ecba2c73176d86e4002d1b0864a8fa8c8f54
Naufal Rizq Ramadhan <91309270+naufalrizq@users.noreply.github.com>
2023-06-26 21:24:38 +0700

    mapping chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,88 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import{OpenAIApi, Configuration} from 'openai'
+import { useState, useEffect } from 'react';
+import { OpenAIApi, Configuration } from 'openai';
 
-const apiKey = "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"
+const apiKey = "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";
 const configuration = new Configuration({ apiKey });
-const openai = new OpenAIApi (configuration);
+const openai = new OpenAIApi(configuration);
 
 function App() {
-  const [input,setInput] = useState("");
-  const [output,setOutput] = useState("");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const response = await openai.createCompletion({
       model: 'text-davinci-003',
       prompt: input,
-      max_tokens:256,
-      temperature:0.9,
-      top_p:1,
-      frequency_penalty:0,
-      presence_penalty:0
+      max_tokens: 256,
+      temperature: 0.9,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0
     });
 
-    setOutput(response.data.choices[0].text);
+    const newMessage = {
+      sender: 'user',
+      content: input
+    };
+
+    const newResponse = {
+      sender: 'gpt',
+      content: response.data.choices[0].text
+    };
+
+    setMessages((prevMessages) => [...prevMessages, newMessage, newResponse]);
+    setInput('');
   };
+
+  useEffect(() => {
+    const savedMessages = localStorage.getItem('messages');
+    if (savedMessages) {
+      setMessages(JSON.parse(savedMessages));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(messages));
+  }, [messages]);
+
   return (
-      <div className='bg-gray-900 text-gray-100 h-screen flex flex-col dark:bg-gray-800 dark:text-gray-50'>
-        <div className='flex-1 overflow-y-scroll'>
-          <div className='flex justify-end mt-2 mr-2'>
-            {/* <div className='bg-green-500 rounded-lg px-4 py-2 text-black max-w-sm'>
-              Selamat datang di Bacain Ai!
-            </div> */}
-          </div>
-          <div className='flex justify-start mt-2 ml-2'>
-            {/* <div className='bg-gray-300 rounded-lg px-4 py-2 text-black max-w-sm'>
-              Bisakah kamu membantuku?
-            </div> */}
-          </div>
-          {
-            output ? (
-              <div className='flex justify-start mt-2 mr-2'>
-              <div className='bg-green-500 rounded-lg px-4 py-2 text-black max-w-sm'>
-                {output}
-              </div>
-            </div>
-            ) : null}
-            <div className='flex justify-end mt-2 mr-2'>
-            <div className='bg-green-500 rounded-lg px-4 py-2 text-black max-w-sm'>
-              {input}
+    <div className='bg-gray-900 text-gray-100 h-screen flex flex-col dark:bg-gray-800 dark:text-gray-50'>
+      <div className='flex-1 overflow-y-scroll'>
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`flex mt-2 ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}
+          >
+            <div
+              className={`bg-${message.sender === 'user' ? 'green' : 'blue'}-500 rounded-lg px-4 py-2 text-black max-w-sm`}
+            >
+              {message.content}
             </div>
           </div>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className='flex items-center'>
-              <input type="text" 
-              className='w-full border rounded-lg py-2 px-4 dark:bg-gray-700 dark:text-gray-200'
-              value={input}
-              onChange={(e) =>  setInput(e.target.value)}
-              placeholder='Ketik disini'
-              />
-              <button type='submit' className='bg-green-500 hover:bg-green-700 rounded-lg px-4 py-2 text-white ml-2'>
-                Kirim
-                </button>
-          </div>
-        </form>
+        ))}
       </div>
-  )
+      <form onSubmit={handleSubmit}>
+        <div className='flex items-center'>
+          <input
+            type='text'
+            className='w-full border rounded-lg py-2 px-4 dark:bg-gray-700 dark:text-gray-200'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder='Ketik disini'
+          />
+          <button
+            type='submit'
+            className='bg-green-500 hover:bg-green-700 rounded-lg px-4 py-2 text-white ml-2'
+          >
+            Kirim
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
